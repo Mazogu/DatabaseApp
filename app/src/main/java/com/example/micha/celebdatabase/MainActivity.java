8a1aeb6dfa332119fbb9a9f254283e1d0191e358
Mazogu <michael_doom@hotmail.com>
2018-01-16 00:26:47 -0500

    Can add celebrities to the database

--- app/src/main/java/com/example/micha/celebdatabase/MainActivity.java
+++ app/src/main/java/com/example/micha/celebdatabase/MainActivity.java
@@ -2,12 +2,49 @@ package com.example.micha.celebdatabase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.micha.celebdatabase.data.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText name;
+    private EditText age;
+    private EditText gender;
+    private EditText industry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.name);
+        age = findViewById(R.id.age);
+        gender = findViewById(R.id.age);
+        industry = findViewById(R.id.age);
+
+
+
+    }
+
+    public void btnCeleb(View view) {
+        String cName, cAge, cGender, cIndustry;
+        DatabaseHelper database = new DatabaseHelper(this);
+        cName = name.getText().toString();
+        cAge = age.getText().toString();
+        cGender = gender.getText().toString();
+        cIndustry = industry.getText().toString();
+        final Celebrity celebrity = new Celebrity(cName, cAge, cGender, cIndustry);
+
+        switch(view.getId()){
+            case R.id.addCeleb:
+                long row = database.saveCeleb(celebrity);
+                Toast.makeText(this,"Saved at row: "+row,Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.celebList:
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/micha/celebdatabase/data/DatabaseHelper.java
+++ app/src/main/java/com/example/micha/celebdatabase/data/DatabaseHelper.java
@@ -1,11 +1,16 @@
 package com.example.micha.celebdatabase.data;
 
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.example.micha.celebdatabase.Celebrity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by micha on 1/15/2018.
@@ -30,6 +35,32 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     public long saveCeleb(Celebrity celeb){
-        return 0;
+        SQLiteDatabase database = getWritableDatabase();
+        ContentValues content = new ContentValues();
+        content.put(DatabaseContract.Celebrity.NAME,celeb.getName());
+        content.put(DatabaseContract.Celebrity.AGE, celeb.getAge());
+        content.put(DatabaseContract.Celebrity.GENDER, celeb.getGender());
+        content.put(DatabaseContract.Celebrity.INDUSTRY, celeb.getIndustry());
+        content.put(DatabaseContract.Celebrity.FAVORITE,celeb.getFavorite());
+
+        long rowNumber = database.insert(DatabaseContract.Celebrity.TABLE_NAME, null, content);
+        database.close();
+        return rowNumber;
     }
+
+    public List<Celebrity> getCelebrityList(){
+        ArrayList<Celebrity> celebList = new ArrayList<>();
+        SQLiteDatabase database = getWritableDatabase();
+        Cursor cursor = database.rawQuery("SELECT * FROM " + DatabaseContract.Celebrity.TABLE_NAME, null);
+
+        if(cursor.moveToFirst()){
+            do{
+                Celebrity celebrity = new Celebrity(cursor.getString(0),cursor.getString(1),cursor.getString(2),cursor.getString(3));
+                celebList.add(celebrity);
+            }while(cursor.moveToNext());
+        }
+        database.close();
+        return celebList;
+    }
+
 }
